63b383bf842ba3664229518661ab65dbef8ce07d
alexcpendleton <alexcpendleton@gmail.com>
2014-04-15 22:25:01 -0400

    Sort of works with hard coded data

---
+++ src/BriansCardBattleGame.Web/Controllers/CardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BriansCardBattleGame.Web.Controllers
+{
+    public class CardController : Controller
+    {
+        //
+        // GET: /Card/
+        public ActionResult Index()
+        {
+            var x = File("~/CardData.json", "application/json");
+            return x;
+        }
+    }
+}
